08aac884936577729286a1ac26edfaae4bbf6a3e
NicoHerlan <herlannicolas6@gmail.com>
2022-09-29 22:29:17 -0300

    Agregar archivos de proyecto.

---
+++ Infrastructure/Persistence/BookingMicroServiceDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class BookingMicroServiceDbContext : DbContext
+    {
+
+        public BookingMicroServiceDbContext(DbContextOptions<BookingMicroServiceDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
